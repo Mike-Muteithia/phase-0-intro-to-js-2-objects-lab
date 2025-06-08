1cb9fa30d63a06d736b2504eec2f59dd94922bd2
Mike Muteithia <mikemuteithia@gmail.com>
2025-06-08 05:46:31 +0300

    Added destructive and nondestructive object functions

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+
+const employee = {
+    name: "Sam Stephenson",
+    streetAddress: "42nd Brooklin Street"
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value ) {
+    return {
+        ...employee,
+        [key]: value,
+    };
+    
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmpolyee = {...employee};
+    delete newEmpolyee[key];
+    return newEmpolyee;
+} 
+
+function destructivelyDeleteFromEmployeeByKey (employee, key) {
+    delete employee[key];
+    return employee;
+}
+
+
+
+
